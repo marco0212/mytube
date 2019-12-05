07ed6b18b4db9464c100b3d4318e8989162efe1e
Jeong <inyeop0212@gmail.com>
2019-12-05 18:24:51 +0900

    Fetching Youtube data from Api & Rendering Video list in Home component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,41 @@ import Home from "./component/Home";
 import Search from "./component/Search";
 import Watch from "./component/Watch";
 import Header from "./component/Header";
+import { getPopularVideos } from "./functions";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      HomeVideos: [],
+      isLoading: true
+    };
+  }
+  componentDidMount() {
+    getPopularVideos(data => {
+      this.setState({ HomeVideos: data, isLoading: false });
+    });
+  }
   render() {
     return (
       <Router>
         <Header />
-        <Switch>
-          <Route path="/search/:keyword" render={() => <Search />} />
-          <Route path="/watch/:id" render={() => <Watch />} />
-          <Route path="/" render={() => <Home />} />
-        </Switch>
+        <main>
+          <Switch>
+            <Route path="/search/:keyword" render={() => <Search />} />
+            <Route path="/watch/:id" render={() => <Watch />} />
+            <Route
+              path="/"
+              render={() =>
+                this.state.isLoading ? (
+                  "Loading"
+                ) : (
+                  <Home HomeVideos={this.state.HomeVideos} />
+                )
+              }
+            />
+          </Switch>
+        </main>
       </Router>
     );
   }
