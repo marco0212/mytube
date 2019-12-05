ee434e2295882f7a9f7ac47f14945cb97d9942cb
Jeong <inyeop0212@gmail.com>
2019-12-05 20:09:35 +0900

    Rendering Watch component #2

--- src/component/Watch/index.js
+++ src/component/Watch/index.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { getVideoById, getYoutubeEmbedUrl } from "../../functions";
+import { Link } from "react-router-dom";
+import {
+  getVideoById,
+  getYoutubeEmbedUrl,
+  getSearchedVideos
+} from "../../functions";
 
 export default class Watch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      currentVideo: {}
+      currentVideo: {},
+      relatedVideo: []
     };
     const { match } = this.props;
     this.id = match.params.id;
@@ -14,15 +20,37 @@ export default class Watch extends React.Component {
   componentDidMount() {
     getVideoById(this.id, data => {
       this.setState({ currentVideo: data, isLoading: false });
+      if (!this.state.isLoading) {
+        getSearchedVideos(this.tag, data => {
+          this.setState({ relatedVideo: data });
+        });
+      }
+    });
+  }
+  componentDidUpdate() {
+    getSearchedVideos(this.tag, data => {
+      this.setState({ relatedVideo: data });
     });
   }
   render() {
+    let video, title, description, time, channelTitle, tags;
     let iframeSrc = getYoutubeEmbedUrl(this.id);
+    if (!this.state.isLoading) {
+      video = this.state.currentVideo.snippet;
+      title = video.title;
+      description = video.description;
+      time = video.publishedAt;
+      channelTitle = video.channelTitle;
+      if (video.tags) {
+        tags = video.tags.slice(0, 5);
+        this.tag = tags[0];
+      }
+    }
     return this.state.isLoading ? (
       "loading"
     ) : (
       <section className="watch">
-        <h2 className="sound-only"></h2>
+        <h2 className="sound-only">{title}</h2>
         <div className="watch-wrap">
           <div className="video-area">
             <div className="frame-wrap">
@@ -30,17 +58,46 @@ export default class Watch extends React.Component {
                 src={iframeSrc}
                 width="560"
                 height="315"
-                title="title"
+                title={title}
                 frameBorder="0"
                 allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen="1"
               />
             </div>
-            <h3></h3>
+            <h3>{title}</h3>
+            <time>{time}</time>
+            <div className="channelInfo-wrap">
+              <div className="img-wrap">
+                <img
+                  src="https://yt3.ggpht.com/a-/AAuE7mC2K6CyIOGWwuOG7yqVcpIS0ZrC1JZDz1FrMg=s68-c-k-c0x00ffffff-no-rj-mo"
+                  alt={channelTitle}
+                />
+              </div>
+              <p>{channelTitle}</p>
+            </div>
+            <div className="videoInfo-wrap">
+              <p>{description}</p>
+              <ul className="related-tags">
+                {tags.map(tag => (
+                  <li key={tag}>
+                    <Link to={`/search/${tag}`}>{tag}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
           <aside>
             <ul>
-              <li>other video</li>
+              {this.state.relatedVideo.map(video => {
+                console.log(video);
+                return (
+                  <li>
+                    <Link to={`/watch/${video.id.videoId}`}>
+                      {video.snippet.title}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </aside>
         </div>
