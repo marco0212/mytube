27433e6f1b7b6396591a5221f8bddaa7cec62786
Jeong <inyeop0212@gmail.com>
2019-12-06 00:11:07 +0900

    Revert commit

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,20 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <header>
-      <h1>
-        <Link to="/">MYTUBE</Link>
-      </h1>
-      <p>
-        <input />
-        <button>
-          <span role="img" aria-label="search-icon">
-            🔍
-          </span>
-        </button>
-      </p>
-    </header>
-  );
+  return "Header";
 }
