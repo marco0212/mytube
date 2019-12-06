c50728809e780366d0a4803e5775c2127c0bd9a7
Jeong <inyeop0212@gmail.com>
2019-12-07 00:44:59 +0900

    Rendering Header component

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,5 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {
-  return "Header";
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="sticky-top bg-danger py-2 shadow">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 col-sm-6 col-md-7">
+              <h1 className="mb-0">
+                <Link className="text-white" to="/">
+                  MYTUBE
+                </Link>
+              </h1>
+            </div>
+            <div className="col-6 col-sm-6 col-md-5 my-auto">
+              <div className="input-group ">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <div className="input-group-append">
+                  <span className="input-group-text">
+                    <FontAwesomeIcon icon={faSearch} size="lg" />
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
