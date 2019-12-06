1669df4ece95dbb321ac26b7a73c89ba7beb91ce
Jeong <inyeop0212@gmail.com>
2019-12-06 22:53:46 +0900

    Rendering Watch component

--- src/Route/Watch/index.js
+++ src/Route/Watch/index.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "../../component/Header";
+import { getYoutubeEmbedUrl } from "../../functions";
 
-export default function Watch() {
-  return <div>Watch</div>;
+export default function Watch(props) {
+  const id = props.match.params.id;
+  console.log(props.location.state.snippet);
+  const { title, description } = props.location.state.snippet;
+  return (
+    <Fragment>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-12">
+            <div className="embed-responsive embed-responsive-16by9">
+              <iframe
+                src={getYoutubeEmbedUrl(id)}
+                title={title}
+                allowFullScreen="1"
+                className="embed-responsive-item"
+              />
+            </div>
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </div>
+          <div className="col-md-4 col-sm-12">aside</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
--- src/component/HomeVideoComponent/index.js
+++ src/component/HomeVideoComponent/index.js
@@ -5,7 +5,7 @@ export default function HomeVideoComponent(props) {
   const { id, snippet } = props;
   return (
     <div className="col-md-4 col-sm-6 col-xs-12">
-      <Link to={{ pathname: `/watch/${id}` }}>
+      <Link to={{ pathname: `/watch/${id}`, state: { snippet } }}>
         <div className="img-wrap">
           <img className="img-fluid" src={snippet.thumbnails.high.url} />
         </div>
