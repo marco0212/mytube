bdd3058f93096d9c5af3eb442f2bb2f2fb36fabc
Jeong <inyeop0212@gmail.com>
2019-12-07 12:59:11 +0900

    Fetching Youtube data in Home

--- src/Route/Home/index.js
+++ src/Route/Home/index.js
@@ -1,5 +1,35 @@
 import React from "react";
-
-export default function Home() {
-  return <div>Home</div>;
+import { getPopularVideos } from "../../functions";
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+  getPopularVideos = () => {
+    getPopularVideos(data => {
+      this.setState({ isLoading: false, videoData: data });
+    });
+  };
+  componentDidMount() {
+    this.getPopularVideos();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? (
+      "Loading..."
+    ) : (
+      <ul>
+        {this.state.videoData.map(data => {
+          return (
+            <li>
+              <img src={data.snippet.thumbnails.default.url} />
+              <p>{data.snippet.title}</p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
