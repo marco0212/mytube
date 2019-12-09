5640aedee78fb43830b7ec0cac796c283d53b72b
Jeong <inyeop0212@gmail.com>
2019-12-09 17:55:20 +0900

    Updates

---
+++ precache-manifest.7517ff2015e782be8ce2e437911437a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c1db12ed97af3462311be8e401cdbe",
+    "url": "/mytube/index.html"
+  },
+  {
+    "revision": "2380d2025d138f9e9149",
+    "url": "/mytube/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "b6b342cb29102025f96c",
+    "url": "/mytube/static/css/main.0e0e26af.chunk.css"
+  },
+  {
+    "revision": "2380d2025d138f9e9149",
+    "url": "/mytube/static/js/2.0d974c9d.chunk.js"
+  },
+  {
+    "revision": "b6b342cb29102025f96c",
+    "url": "/mytube/static/js/main.1acb0fd3.chunk.js"
+  },
+  {
+    "revision": "d20984b42509c0aa2cad",
+    "url": "/mytube/static/js/runtime-main.77384829.js"
+  }
+]);
