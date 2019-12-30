2ba7a8354ae65858cb52f7b7ee2cc5d5dbd54ead
Jeong <inyeop0212@gmail.com>
2019-12-30 23:12:48 +0900

    Fix markup

--- src/Component/PopularList.js
+++ src/Component/PopularList.js
@@ -5,9 +5,11 @@ export default function PopularList({ videos }) {
   return (
     <div>
       <h3>PopularList</h3>
-      {videos.map(video => (
-        <VideoBox video={video} key={video.id} />
-      ))}
+      <ul>
+        {videos.map(video => (
+          <VideoBox video={video} key={video.id} />
+        ))}
+      </ul>
     </div>
   );
 }
