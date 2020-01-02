110b428aa9396db3bf6721d1e853a9b8ac99727f
Jeong <inyeop0212@gmail.com>
2020-01-02 22:47:42 +0900

    RESET

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Route/Home";
-import Watch from "./Route/Watch";
-import Header from "./Component/Header";
-import GlobalStyle from "./style/GlobalStyle";
-import { theme } from "./style/theme";
-import { ThemeProvider } from "styled-components";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <GlobalStyle />
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path={`/watch/:id`} component={Watch} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
-  );
+  return <div>App</div>;
 }
--- src/Component/RelateVideoList.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect } from "react";
-
-export default function RelateVideoList({ id }) {
-  useEffect(() => {}, [id]);
-  return <aside>asdf</aside>;
-}
