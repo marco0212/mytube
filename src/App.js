92b876567dba605a0e6db26e3f4153031a745b66
Jeong <inyeop0212@gmail.com>
2020-01-03 08:45:05 +0900

    Setup initial Route & Create Home Route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import Theme from "./Style/Theme";
 import { GlobalStyles } from "./Style/GlobalStyles";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Route/Home";
 
 export default function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <div>App</div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
